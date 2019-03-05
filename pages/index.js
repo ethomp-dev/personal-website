55eee1fe35c6fc928b053a023c91f0cc3146710d
ethomp54 <ethomp54@uncc.edu>
2019-03-05 03:17:38 -0500

    update hero description

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default () => (
 
                     <h1>Hi, I'm a designer and developer.</h1>
 
-                    <p className='subtitle is-flex-hidden-mobile'>Focused on creating web applications with enhanced user experience, currently looking for a full-time role.</p>
+                    <p className='subtitle is-flex-hidden-mobile'>Focused on creating beautifully architected web applications with enhanced user experience</p>
 
                     <ScrollButton text='Explore Portfolio' />
 
